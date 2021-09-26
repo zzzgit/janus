a052a33ed124181ed9be29a9cdd5f13ef9eb8a11
jonkee <zzzgoo@gmail.com>
2021-09-26 20:48:36 +0800

    bugs

--- array.js
+++ array.js
@@ -3,7 +3,7 @@ module.exports = {
 	"rules": {
 		"array-func/from-map": "error",
 		"array-func/no-unnecessary-this-arg": "error",
-		"array-func/prefer-array-from": "error",
+		"array-func/prefer-array-from": "off",
 		"array-func/avoid-reverse": "error",
 		"array-func/prefer-flat-map": "error",
 		"array-func/prefer-flat": "error",
