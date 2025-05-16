37ddb5594770b60f3a8b16ee8f2d82146763251a
chris <chris@home>
2025-05-16 16:22:58 +0800

    add comment to ts.js

--- ts.js
+++ ts.js
@@ -1,7 +1,12 @@
+// 目前的策略：使用js的stylistic规则，不用ts的stylistic规则
+
 // import stylisticTs from '@stylistic/eslint-plugin-ts'
 import tsParser from '@typescript-eslint/parser'
 import js from './js.js'
 import tseslint from 'typescript-eslint'
+
+// exclude stylistic/js from '@stylistic/eslint-plugin-js'	
+// const jsCopy = js.filter(item => item.name !== 'janus/stylistic/js')
 
 export default [
 	...js,
