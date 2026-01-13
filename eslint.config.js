7d65671ed900653bd09f9b35a10097b2dd4eed28
chris <chris@home>
2026-01-13 13:36:24 +0800

    remove css

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,7 @@
 import js from './test/js/eslint.js.js'
-import css from './test/jsx/eslint.css.js'
 import react from './test/jsx/eslint.jsx.js'
 import node from './test/node/eslint.node.js'
 import ts from './test/ts/eslint.ts.js'
 
-export default [...js, ...react, ...css, ...node, ...ts]
-// export default [...jsify(js), ...jsify(react), ...jsify(css), ...jsify(node), ...tsify(ts)]
+export default [...js, ...react,  ...node, ...ts]
+// export default [...jsify(js), ...jsify(react), ...jsify(node), ...tsify(ts)]
--- test/jsx/eslint.css.js
+++
@@ -1,9 +0,0 @@
-import css from '../../css.js'
-
-export default [
-
-	...css.map(config=> ({
-		...config,
-		files: ['test/jsx/**/*.jsx'],
-	})),
-]
