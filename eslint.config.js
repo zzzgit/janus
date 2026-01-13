f4925cf8fba11c2da4cb2587f0bf034ed333410e
chris <chris@home>
2026-01-13 13:41:24 +0800

    update the dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,5 @@ import react from './test/jsx/eslint.jsx.js'
 import node from './test/node/eslint.node.js'
 import ts from './test/ts/eslint.ts.js'
 
-export default [...js, ...react,  ...node, ...ts]
+export default [...js, ...react, ...node, ...ts]
 // export default [...jsify(js), ...jsify(react), ...jsify(node), ...tsify(ts)]
